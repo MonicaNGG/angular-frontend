29d27d9e9fb48bb5fb9d489c2903b9c8a488437e
MonicaNGG <mgallegog@ucentral.edu.co>
2024-05-19 23:18:50 -0500

    Env

--- src/app/enviroment/enviroments.ts
+++ src/app/enviroment/enviroments.ts
@@ -1,6 +1,6 @@
 
 
 export const enviroment = {
-    //BASE_URL: 'https://backendmikaza.onrender.com/api/',
-    BASE_URL: 'http://localhost:8080/api/'
+    BASE_URL: 'https://backendmikaza.onrender.com/api/',
+    // BASE_URL: 'http://localhost:8080/api/'
 }
