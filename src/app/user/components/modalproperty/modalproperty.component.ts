f4855832dd76326871af9f91a233f95eaa73f478
MonicaNGG <mgallegog@ucentral.edu.co>
2024-05-21 23:08:17 -0500

    Adding button

--- src/app/user/components/modalproperty/modalproperty.component.ts
+++ src/app/user/components/modalproperty/modalproperty.component.ts
@@ -2,6 +2,7 @@ import { Component, Input } from '@angular/core';
 import { Inmueble } from '../../../interfaces/Inmueble';
 import { CommonModule } from '@angular/common';
 import { ModalService } from '../../../services/modal.service';
+import { ToastService } from '../../../services/toast.service';
 
 @Component({
   selector: 'app-modalproperty',
@@ -15,7 +16,7 @@ export class ModalpropertyComponent {
 
   item?: Inmueble;  
 
-  constructor( private modalServ: ModalService ){}
+  constructor( private modalServ: ModalService, private toastSert: ToastService ){}
 
   ngOnInit(): void {
 
